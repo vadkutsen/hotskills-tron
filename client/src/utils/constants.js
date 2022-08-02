b28020f285cc3512382dd5bd4a81e616cf152e05
vadkutsen <vadkutsen@gmail.com>
2022-08-02 12:05:05 +0200

    Revert last change

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from "./contractABI.json";
 
-export const contractAddress = "TEtNyFZPPNsrYrVS7dW6deRBUc7MA6sX2Q";
+export const contractAddress = "TFXueB5e9FzotHmCQccWULSsgnNnn41Wby";
 export const contractABI = abi.abi;
