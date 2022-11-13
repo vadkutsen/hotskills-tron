d7361ebf1299841b902cf4b8128eaccd5eacf344
vadkutsen <vadkutsen@gmail.com>
2022-11-13 23:25:03 +0100

    Fix avatar size

--- client/src/components/services/ServiceCard.jsx
+++ client/src/components/services/ServiceCard.jsx
@@ -39,16 +39,16 @@ const ServiceCard = ({
 
   return (
     <Link to={`/services/${id}`}>
-      <div className="w-[20rem] h-[30rem] flex flex-col justify-start text-white white-glassmorphism p-3 m-2 cursor-pointer transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-104 duration-300">
+      <div className="w-[20rem] h-[30rem] flex flex-col justify-between text-white white-glassmorphism p-3 m-2 cursor-pointer transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-104 duration-300">
         <img alt="Service" className="self-center rounded-md" src={image} />
         <div className="flex flex-col w-full">
           <div className="mt-2 flex flex-row justify-between">
             <p className="text-xl truncate ...">{title}</p>
-            <p className="text-xl text-left">{price} TRX</p>
+            {/* <p className="text-xl text-left">{price} TRX</p> */}
           </div>
-          <div className="flex flex-row items-center">
+          <div className="flex flex-row mt-2 mb-2 items-center">
             {profile && profile.avatar ? (
-              <img alt="Avatar" className="" src={profile.avatar} />
+              <img alt="Avatar" className="w-[2.5rem] mr-1 rounded-full border" src={profile.avatar} />
             ) : (
               <AutoAvatar userId={author} size={36} />
             )}
@@ -64,6 +64,7 @@ const ServiceCard = ({
             </div>
           </div>
         </div>
+        <p className="text-xl mt-20 self-end">{price} TRX</p>
       </div>
     </Link>
   );
